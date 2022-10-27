f4a84749fe2ba654a5d7dc5ca9779dcac3780d15
Lux <anguyen780@gmail.com>
2022-10-27 18:12:38 -0500

    fixed bug where header wasnt appearing and content wasnt saving on reload

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
+  const request = store.put({ content });
   const result = await request;
   console.log(result);
 }
@@ -30,7 +30,6 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log(result);
-  return result;
 }
 
 initdb();
